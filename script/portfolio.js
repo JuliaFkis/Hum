2683f97a52c5441e67aa9c1a348da870dba92ac8
fkiseleva01 <fkiseleva01@gmail.com>
2021-08-04 19:28:29 +0300

    add filter

---
+++ script/portfolio.js
@@ -0,0 +1,23 @@
+let navFilterPortfolio = document.querySelectorAll(".portfolio-item")
+
+for(let item of navFilterPortfolio){
+    item.addEventListener("click", ()=>{
+
+    const navData = item.getAttribute("data-tab-name")
+    let portfolioImg = document.querySelectorAll(".portfolio-card")
+
+    portfolioImg.forEach((i)=>{
+        if(navData === "all"){
+            i.classList.remove("hide")
+        } else {
+            if (i.getAttribute("data-tab-name") !== navData) {
+            i.classList.add("hide") 
+        } else {
+            i.classList.remove("hide")
+        }
+    }
+    })
+
+})
+  
+}
