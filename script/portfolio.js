c821a4d6a9131c536a706de6b58be3228a7232aa
fkiseleva01 <fkiseleva01@gmail.com>
2021-08-09 21:02:45 +0300

    add changes for portfolio button

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,43 +1,52 @@
 let navFilterPortfolio = document.querySelectorAll(".portfolio-item")
+let portfolioImg = document.querySelectorAll(".portfolio-card")
+
 
 for(let item of navFilterPortfolio){
     item.addEventListener("click", ()=>{
 
     const navData = item.getAttribute("data-tab-name")
-    let portfolioImg = document.querySelectorAll(".portfolio-card")
+    portfolioImg = document.querySelectorAll(".portfolio-card")
 
     portfolioImg.forEach((i)=>{
         if(navData === "all"){
             i.classList.remove("hide")
         } else {
             if (i.getAttribute("data-tab-name") !== navData) {
-            i.classList.add("hide") 
-        } else {
-            i.classList.remove("hide")
+                i.classList.add("hide") 
+            } else {
+                i.classList.remove("hide")
+            }
         }
-    }
     })
 })
 }
 
 
 // Обсудить с ментором ошибку в кнопке 
+// const btn = document.querySelector(".portfolio-btn")
+// btn.addEventListener("click",()=>{
+//     portfolioImg.forEach((i)=>{
+//        i.classList.toggle("hide")
+//     })
+// })
+
 const btn = document.querySelector(".portfolio-btn")
+
+let hidenPortfolioImg = document.querySelectorAll(".portfolio-card")
+hidenPortfolioImg = Array.from(hidenPortfolioImg)
+hidenPortfolioImg.slice(12)
+console.log(hidenPortfolioImg);
+
+
 btn.addEventListener("click",()=>{
-    portfolioImg.forEach((i)=>{
-        if(i === true){
-            i[12,24].classList.remove("hide")
+    hidenPortfolioImg.forEach((i)=>{
+        if(!"click"){
+            i.classList.add("hide")
         } else {
-            i[12,24].classList.add("hide")
+            i.classList.remove("hide")
         }
-        })
+        console.log(i);
+        
     })
-
-
-
-
-
-
-
-
-
+})
